46d58bcf96962c90e0ac4f85d0034d2e9fcae91d
Vas2306 <113250146+Vas2306@users.noreply.github.com>
2023-03-05 20:12:48 +0000

    Deploying to gh-pages from @ Vas2306/goit-js-hw-09@6f533c909a2da221a43b3fd7d7ba08005f334256 🚀

---
+++ 01-color-switcher.abd3b975.js
@@ -0,0 +1,2 @@
+const t={btnStart:document.querySelector("button[data-start]"),btnStop:document.querySelector("button[data-stop]"),body:document.body};let n=null;function e(){t.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}t.btnStart.disabled=!1,t.btnStop.disabled=!0,t.btnStart.addEventListener("click",(function(){n=setInterval(e,1e3),t.btnStart.disabled=!0,t.btnStop.disabled=!1})),t.btnStop.addEventListener("click",(function(){t.btnStart.disabled=!1,t.btnStop.disabled=!0,clearInterval(n)}));
+//# sourceMappingURL=01-color-switcher.abd3b975.js.map
